e7e31bddbfb7024d771b84439ad8c4f6cce092ea
Fasaani <niko.luoto@gmail.com>
2015-08-21 16:59:34 +0300

    Pelikenttä
    Peilkkää kokeilu tässä vaiheessa

---
+++ MonsterBox/Assets/Scripts/liike.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class liike : MonoBehaviour 
+{
+	public float speed = 10f;
+	public int aika = 0;
+
+	void Update () 
+	{
+
+		if (aika>=0
+		    && aika<100)
+		{
+
+			transform.Translate(Vector2.right * speed * Time.deltaTime);
+			aika++;
+		}
+
+		if (aika>=100
+		    && aika<200)
+		{
+			transform.Translate(Vector2.left * speed * Time.deltaTime);
+			aika++;
+		}
+
+		if (aika>=200
+		    || aika<0)
+		{
+			aika=0;
+		}
+	}
+}
