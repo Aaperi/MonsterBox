537a1099fffb9c67270aa9435e465575de853bda
Eonii <tasplund90@gmail.com>
2015-10-18 23:41:43 +0300

    Main Menu Created

---
+++ MonsterBox/Assets/Resources/Scripts/MenuScripts/MainMenu.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+    public Button playButton;
+    public Button optionsButton;
+    public Button storeButton;
+    public Button creditsButton;
+    public Button quitButton;
+
+    public Canvas mainMenu;
+    public Canvas optionsMenu;
+    public Canvas creditsMenu;
+
+    void Start () {
+
+        mainMenu = mainMenu.GetComponent<Canvas>();
+
+        playButton = playButton.GetComponent<Button>();
+        optionsButton = playButton.GetComponent<Button>();
+        storeButton = playButton.GetComponent<Button>();
+        creditsButton = playButton.GetComponent<Button>();
+        quitButton = playButton.GetComponent<Button>();
+
+        optionsMenu.enabled = false;
+        creditsMenu.enabled = false;
+
+
+
+    }
+
+    public void BackPressed()
+    {
+        if (optionsMenu)
+        {
+            optionsMenu.enabled = false;
+        }
+        if (creditsMenu)
+        {
+            creditsMenu.enabled = false;
+        }
+
+
+    }
+
+    public void playPress(){
+        Application.LoadLevel("World1");
+    }
+
+    public void optionsPress(){
+        optionsMenu.enabled = true;
+    }
+
+    public void storePress(){
+        Application.LoadLevel("Store");
+    }
+
+    public void creditsPress(){
+        creditsMenu.enabled = true;
+    }
+
+    public void quitPress(){
+        Application.Quit();
+    }
+
+}
