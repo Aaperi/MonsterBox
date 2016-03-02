1dbdc9a596cc878b2c3d55a914b08d78e5b9b634
Masi Niemi <mniemi90@gmail.com>
2016-03-02 22:30:52 +0200

    level 1 things
    first level ground, camera movement script, breaking platform script and
    sprites

---
+++ MonsterBox/Assets/Resources/Scripts/PlatformScripts/BrokeningPlatform.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BrokeningPlatform : MonoBehaviour
+{
+    public float BreakTime = 10f;
+    public bool startBreak = false;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (BreakTime > 0 && startBreak)
+        {
+            BreakTime -= Time.deltaTime;
+        }
+        else if(BreakTime <= 0 && startBreak)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    void OnCollisionEnter2D(Collision2D coll)
+    { 
+        //Debug.LogError("enter");
+        startBreak = true;
+    }
+}
---
+++ MonsterBox/Assets/Resources/Scripts/cameramovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameramovement : MonoBehaviour {
+    public Vector3 offsets;
+    public GameObject player;
+    private Transform playertrans;
+	// Use this for initialization
+	void Start () {
+        playertrans = player.transform;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        this.transform.position = new Vector3(playertrans.position.x + offsets.x, playertrans.position.y + offsets.y, offsets.z);
+	}
+}
